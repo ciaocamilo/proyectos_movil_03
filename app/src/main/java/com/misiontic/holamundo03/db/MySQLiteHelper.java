d2dec1056ee7e6c010baf47127fe4ffa64ae99f4
Camilo A <ing_camiloacg@hotmail.com>
2021-11-22 09:04:21 -0500

    Crea conexión a base de datos e inserta en tabla personas

--- app/src/main/java/com/misiontic/holamundo03/db/MySQLiteHelper.java
+++ app/src/main/java/com/misiontic/holamundo03/db/MySQLiteHelper.java
@@ -8,17 +8,30 @@ import androidx.annotation.Nullable;
 
 public class MySQLiteHelper extends SQLiteOpenHelper {
 
-    public MySQLiteHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    private static final String DB_NAME = "app_03_db.sqlite";
+    private static final int DB_VERSION = 1;
+
+    private static final String PEOPLE_TABLE_CREATE = "CREATE TABLE personas(_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                                                        "nombres TEXT, apellidos TEXT, direccion TEXT, telefono TEXT, " +
+                                                        "fecha_nacimiento DATE, foto TEXT)";
+
+    public MySQLiteHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(PEOPLE_TABLE_CREATE);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
 
     }
+
+    public void insertData(String sentence) {
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL(sentence);
+    }
+
 }
