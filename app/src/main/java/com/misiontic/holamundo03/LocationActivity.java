06bb0f2ab0fefb993eafa9826d6844053fff8cf4
Camilo A <ing_camiloacg@hotmail.com>
2021-12-02 10:23:59 -0500

    Agrega ícono a la App

--- app/src/main/java/com/misiontic/holamundo03/LocationActivity.java
+++ app/src/main/java/com/misiontic/holamundo03/LocationActivity.java
@@ -4,7 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+
 public class LocationActivity extends AppCompatActivity {
+
+    /*
+    private static final int RC_SIGN_IN = 9001;
+
+    private GoogleSignInClient mSignInClient;
+
+     */
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
